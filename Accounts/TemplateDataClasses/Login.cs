a793f073a4f7871571c100ea35b3da4dd7fe13e4
That-Cool-Coder <62006241+That-Cool-Coder@users.noreply.github.com>
2021-08-21 16:18:48 +0930

    try to fix accounts but break it

---
+++ Accounts/TemplateDataClasses/Login.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Accounts.TemplateDataClasses
+{
+    public class Login : Base
+    {
+        public string username;
+        public string password;
+    }
+}
